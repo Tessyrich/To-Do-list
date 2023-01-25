6ec12b29b827d0634f6defbacdb28ce5f007b677
Tessyrich <olyrich3@gmail.com>
2023-01-25 02:38:28 -0600

    Add book structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import './style.css';
+import showTodo from '../modules/showTodo.js';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+// Lodash, now imported by this script
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
+window.addEventListener('DOMContentLoaded', () => {
+  showTodo();
+});
