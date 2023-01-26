d1217e4251f45f736d1b1cf792e6ec4a2cea7a08
Tessyrich <olyrich3@gmail.com>
2023-01-26 04:59:42 -0600

    Implement add remove

--- modules/showTodo.js
+++ modules/showTodo.js
@@ -1,12 +1,15 @@
-import todos from './todoData.js';
+// import todos from './todoData.js';
 
-const showTodo = () => {
-  todos.sort((a, b) => a.index - b.index);
-  const todoList = document.querySelector('.todo-list');
-  const todoApp = todos.map((todo) => `        
-        <li id=${todo.index}><input type="checkbox">${todo.description} <i class="fas fa-ellipsis-v"></i> </li>        
-      `);
-  todoList.innerHTML = todoApp.join('');
-};
+// const showTodo = () => {
+//   todos.sort((a, b) => a.index - b.index);
+//   const todoList = document.querySelector('.todo-list');
+//   todoList.innerHTML = todoApp.join('');
+//   const todoApp = todos.map(
+//     (todo) => `        
+//         <li id=${todo.index}><input type="checkbox">${todo.description} <i class="fas fa-ellipsis-v dot main"></i><i class="fas fa-trash-alt delete main"></i> </li>        
+//       `
+//   );
+//   todoList.innerHTML = todoApp.join('');
+// };
 
-export default showTodo;
+// export default showTodo;
--- modules/todoData.js
+++ modules/todoData.js
@@ -1,19 +1,5 @@
-const todos = [
-  {
-    index: 1,
-    description: 'Take out the trash',
-    Completed: true,
-  },
-  {
-    index: 2,
-    description: 'Fix my old car',
-    Completed: false,
-  },
-  {
-    index: 3,
-    description: 'Visit my friend',
-    Completed: true,
-  },
-];
+// let todos = [
 
-export default todos;
+// ];
+
+// export default todos;
